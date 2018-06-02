2db66b94092742f6fa7e293f132cc959ef9fb079
tianjj <527623439@qq.com>
2018-06-02 20:36:25 +0800

    add test usage sample codes.

--- src/test/Main.kt
+++ src/test/Main.kt
@@ -1,5 +1,25 @@
 package test
 
-fun main(args: Array<String>) {
+import subscription.action.Action
+import subscription.action.ActionListener
 
+fun main(args: Array<String>) {
+    testAction()
+
+}
+
+fun testAction(){
+    var actionListener = object : ActionListener<String>{
+        override fun notifyMessage(msg: String) {
+            print(msg)
+        }
+    }
+
+var action  = Action<String>()
+
+    action += actionListener /*register*/
+    action.notify("test message after register")
+
+    action -= actionListener/*unregister*/
+    action.notify("test message after unregister")
 }
